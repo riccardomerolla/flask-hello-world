64e5ad4914178f687281964d5963e145cd1c9bde
Riccardo Merolla <riccardo.merolla@hey.com>
2023-01-27 17:52:25 +0100

    add bolt

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
-app = Flask(__name__)
+import logging
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+logging.basicConfig(level=logging.DEBUG)
+
+from slack_bolt import App
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+app = App()
+
+
+@app.middleware  # or app.use(log_request)
+def log_request(logger, body, next):
+    logger.debug(body)
+    return next()
+
+
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
+
+@app.event("message")
+def handle_message():
+    pass
+
+
+from flask import Flask, request
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
+
+
+@flask_app.route("/echo", methods=["GET"])
+def echo():
+    return "Hello, SLACK-APP-BOLT!"
